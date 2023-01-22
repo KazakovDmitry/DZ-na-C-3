c240c444aaa32873b84bed6c578c61f1da16ccdf
Dmitry <dimakameya@gmail.com>
2023-01-22 12:30:07 +0500

    Таблица кубов от 1 до введёного числа

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message){
+        int result = 0;
+    while (true){
+            Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result)){
+            break;
+        }        
+        else{    
+            Console.WriteLine("Вы ввели некорректное число");
+        }
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число: ");
+Console.WriteLine($"Таблица кубов чисел от 1 до {number}: ");
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(i * i * i);
+}
